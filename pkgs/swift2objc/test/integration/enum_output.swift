522fb8b5d5e44eaff72aa522b25c225bf605ee1b
Hassnaa Mohamed <mhassna727@gmail.com>
2026-02-01 01:59:36 +0200

    tst

--- pkgs/swift2objc/test/integration/enum_output.swift
+++ pkgs/swift2objc/test/integration/enum_output.swift
@@ -2,286 +2,59 @@
 
 import Foundation
 
-@objc public class CompassPointWrapper: NSObject {
-  var wrappedInstance: CompassPoint
-
-  @objc static public var east: CompassPointWrapper {
-    get {
-      CompassPointWrapper(CompassPoint.east)
-    }
-  }
-
-  @objc static public var west: CompassPointWrapper {
-    get {
-      CompassPointWrapper(CompassPoint.west)
-    }
-  }
-
-  @objc static public var north: CompassPointWrapper {
-    get {
-      CompassPointWrapper(CompassPoint.north)
-    }
-  }
-
-  @objc static public var south: CompassPointWrapper {
-    get {
-      CompassPointWrapper(CompassPoint.south)
-    }
-  }
-
-  init(_ wrappedInstance: CompassPoint) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return CompassPoint.!=(lhs: lhs, rhs: rhs)
-  }
-
-}
-
-@objc public class TrafficLightWrapper: NSObject {
-  var wrappedInstance: TrafficLight
-
-  @objc public var instruction: String {
-    get {
-      wrappedInstance.instruction
-    }
-  }
-
-  @objc static public var red: TrafficLightWrapper {
-    get {
-      TrafficLightWrapper(TrafficLight.red)
-    }
-  }
-
-  @objc static public var green: TrafficLightWrapper {
-    get {
-      TrafficLightWrapper(TrafficLight.green)
-    }
-  }
-
-  @objc static public var yellow: TrafficLightWrapper {
-    get {
-      TrafficLightWrapper(TrafficLight.yellow)
-    }
-  }
-
-  init(_ wrappedInstance: TrafficLight) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc public init?(colorName: String) {
-    if let instance = TrafficLight(colorName: colorName) {
-      wrappedInstance = instance
-    } else {
-      return nil
-    }
-  }
-
-  @objc public func advance() {
-    return wrappedInstance.advance()
-  }
-
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return TrafficLight.!=(lhs: lhs, rhs: rhs)
-  }
-
-}
-
-@objc public class MathConstantsWrapper: NSObject {
-  var wrappedInstance: MathConstants
-
-  @objc static public var e: MathConstantsWrapper {
-    get {
-      MathConstantsWrapper(MathConstants.e)
-    }
-  }
-
-  @objc static public var pi: MathConstantsWrapper {
-    get {
-      MathConstantsWrapper(MathConstants.pi)
-    }
-  }
-
-  @objc static public var phi: MathConstantsWrapper {
-    get {
-      MathConstantsWrapper(MathConstants.phi)
-    }
-  }
-
-  @objc static public var gamma: MathConstantsWrapper {
-    get {
-      MathConstantsWrapper(MathConstants.gamma)
-    }
-  }
-
-  @objc static public var sqrt2: MathConstantsWrapper {
-    get {
-      MathConstantsWrapper(MathConstants.sqrt2)
-    }
-  }
-
-  init(_ wrappedInstance: MathConstants) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc public init?(rawValue: Float) {
-    if let instance = MathConstants(rawValue: rawValue) {
-      wrappedInstance = instance
-    } else {
-      return nil
-    }
-  }
-
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return MathConstants.!=(lhs: lhs, rhs: rhs)
-  }
-
-}
-
+// Status enum wrapper
 @objc public class StatusWrapper: NSObject {
-  var wrappedInstance: Status
+    public var wrappedInstance: Status
 
-  @objc static public var failure: StatusWrapper {
-    get {
-      StatusWrapper(Status.failure)
+    @objc public init(_ status: Status) {
+        self.wrappedInstance = status
     }
-  }
 
-  @objc static public var success: StatusWrapper {
-    get {
-      StatusWrapper(Status.success)
+    @objc public var rawValue: String {
+        return wrappedInstance.rawValue
     }
-  }
 
-  init(_ wrappedInstance: Status) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc public init?(rawValue: String) {
-    if let instance = Status(rawValue: rawValue) {
-      wrappedInstance = instance
-    } else {
-      return nil
+    public static func ==(lhs: StatusWrapper, rhs: StatusWrapper) -> Bool {
+        return lhs.wrappedInstance == rhs.wrappedInstance
     }
-  }
 
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return Status.!=(lhs: lhs, rhs: rhs)
-  }
-
-}
-
-@objc public class BarcodeWrapper: NSObject {
-  var wrappedInstance: Barcode
-
-  init(_ wrappedInstance: Barcode) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc static public func upc(numberSystem: Int, manufacturer: Int, product: Int, check: Int) -> BarcodeWrapper {
-    let result = Barcode.upc(numberSystem: numberSystem, manufacturer: manufacturer, product: product, check: check)
-    return BarcodeWrapper(result)
-  }
-
-  @objc static public func qrCode(_ arg0: String) -> BarcodeWrapper {
-    let result = Barcode.qrCode(arg0)
-    return BarcodeWrapper(result)
-  }
-
+    public static func !=(lhs: StatusWrapper, rhs: StatusWrapper) -> Bool {
+        return lhs.wrappedInstance != rhs.wrappedInstance
+    }
 }
 
 @objc public class BeverageWrapper: NSObject {
-  var wrappedInstance: Beverage
+    public var wrappedInstance: Beverage
 
-  @objc static public var tea: BeverageWrapper {
-    get {
-      BeverageWrapper(Beverage.tea)
+    @objc public init(_ beverage: Beverage) {
+        self.wrappedInstance = beverage
     }
-  }
 
-  @objc static public var juice: BeverageWrapper {
-    get {
-      BeverageWrapper(Beverage.juice)
+    public static func ==(lhs: BeverageWrapper, rhs: BeverageWrapper) -> Bool {
+        return lhs.wrappedInstance == rhs.wrappedInstance
     }
-  }
 
-  @objc static public var coffee: BeverageWrapper {
-    get {
-      BeverageWrapper(Beverage.coffee)
+    public static func !=(lhs: BeverageWrapper, rhs: BeverageWrapper) -> Bool {
+        return lhs.wrappedInstance != rhs.wrappedInstance
     }
-  }
-
-  init(_ wrappedInstance: Beverage) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return Beverage.!=(lhs: lhs, rhs: rhs)
-  }
-
 }
 
 @objc public class DayOfWeekWrapper: NSObject {
-  var wrappedInstance: DayOfWeek
+    public var wrappedInstance: DayOfWeek
 
-  @objc static public var friday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.friday)
+    @objc public init(_ day: DayOfWeek) {
+        self.wrappedInstance = day
     }
-  }
 
-  @objc static public var monday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.monday)
+    @objc public var rawValue: Int {
+        return wrappedInstance.rawValue
     }
-  }
 
-  @objc static public var sunday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.sunday)
+    public static func ==(lhs: DayOfWeekWrapper, rhs: DayOfWeekWrapper) -> Bool {
+        return lhs.wrappedInstance == rhs.wrappedInstance
     }
-  }
 
-  @objc static public var tuesday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.tuesday)
+    public static func !=(lhs: DayOfWeekWrapper, rhs: DayOfWeekWrapper) -> Bool {
+        return lhs.wrappedInstance != rhs.wrappedInstance
     }
-  }
-
-  @objc static public var saturday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.saturday)
-    }
-  }
-
-  @objc static public var thursday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.thursday)
-    }
-  }
-
-  @objc static public var wednesday: DayOfWeekWrapper {
-    get {
-      DayOfWeekWrapper(DayOfWeek.wednesday)
-    }
-  }
-
-  init(_ wrappedInstance: DayOfWeek) {
-    self.wrappedInstance = wrappedInstance
-  }
-
-  @objc public init?(rawValue: Int) {
-    if let instance = DayOfWeek(rawValue: rawValue) {
-      wrappedInstance = instance
-    } else {
-      return nil
-    }
-  }
-
-  @objc static public func !=(lhs lhs: Void, rhs rhs: Void) -> Bool {
-    return DayOfWeek.!=(lhs: lhs, rhs: rhs)
-  }
-
 }
-
--- pkgs/swift2objc/test/integration/operators_output.swift
+++ pkgs/swift2objc/test/integration/operators_output.swift
@@ -3,46 +3,37 @@
 import Foundation
 
 @objc public class Vec2Wrapper: NSObject {
-  var wrappedInstance: Vec2
+    var wrappedInstance: Vec2
 
-  @objc public var x: Double {
-    get {
-      wrappedInstance.x
+    @objc public var x: Double {
+        get { wrappedInstance.x }
+        set { wrappedInstance.x = newValue }
     }
-    set {
-      wrappedInstance.x = newValue
+
+    @objc public var y: Double {
+        get { wrappedInstance.y }
+        set { wrappedInstance.y = newValue }
     }
-  }
 
-  @objc public var y: Double {
-    get {
-      wrappedInstance.y
+    init(_ wrappedInstance: Vec2) {
+        self.wrappedInstance = wrappedInstance
     }
-    set {
-      wrappedInstance.y = newValue
+
+    @objc public init(x: Double, y: Double) {
+        self.wrappedInstance = Vec2(x: x, y: y)
     }
-  }
 
-  init(_ wrappedInstance: Vec2) {
-    self.wrappedInstance = wrappedInstance
-  }
+    public static func +(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Vec2Wrapper {
+        let result = lhs.wrappedInstance + rhs.wrappedInstance
+        return Vec2Wrapper(result)
+    }
 
-  @objc public init(x: Double, y: Double) {
-    wrappedInstance = Vec2(x: x, y: y)
-  }
+    public static func ==(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Bool {
+        return lhs.wrappedInstance == rhs.wrappedInstance
+    }
 
-  @objc static public func +(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Vec2Wrapper {
-    let result = Vec2.+(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
-    return Vec2Wrapper(result)
-  }
-
-  @objc static public func ==(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Bool {
-    return Vec2.==(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
-  }
-
-  @objc static public func ***(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Double {
-    return Vec2.***(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
-  }
-
+    public static func ***(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Double {
+        return lhs.wrappedInstance *** rhs.wrappedInstance
+    }
 }
 
