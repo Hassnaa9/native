c34f94715c99f6ca9d97ef7b08c902c070848092
Hassnaa Mohamed <mhassna727@gmail.com>
2026-02-01 02:12:45 +0200

    tst

--- pkgs/swift2objc/test/integration/operators_output.swift
+++ pkgs/swift2objc/test/integration/operators_output.swift
@@ -3,37 +3,46 @@
 import Foundation
 
 @objc public class Vec2Wrapper: NSObject {
-    var wrappedInstance: Vec2
+  var wrappedInstance: Vec2
 
-    @objc public var x: Double {
-        get { wrappedInstance.x }
-        set { wrappedInstance.x = newValue }
+  @objc public var x: Double {
+    get {
+      wrappedInstance.x
     }
+    set {
+      wrappedInstance.x = newValue
+    }
+  }
 
-    @objc public var y: Double {
-        get { wrappedInstance.y }
-        set { wrappedInstance.y = newValue }
+  @objc public var y: Double {
+    get {
+      wrappedInstance.y
     }
+    set {
+      wrappedInstance.y = newValue
+    }
+  }
 
-    init(_ wrappedInstance: Vec2) {
-        self.wrappedInstance = wrappedInstance
-    }
+  init(_ wrappedInstance: Vec2) {
+    self.wrappedInstance = wrappedInstance
+  }
 
-    @objc public init(x: Double, y: Double) {
-        self.wrappedInstance = Vec2(x: x, y: y)
-    }
+  @objc public init(x: Double, y: Double) {
+    wrappedInstance = Vec2(x: x, y: y)
+  }
 
-    public static func +(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Vec2Wrapper {
-        let result = lhs.wrappedInstance + rhs.wrappedInstance
-        return Vec2Wrapper(result)
-    }
+  @objc static public func +(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Vec2Wrapper {
+    let result = Vec2.+(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
+    return Vec2Wrapper(result)
+  }
 
-    public static func ==(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Bool {
-        return lhs.wrappedInstance == rhs.wrappedInstance
-    }
+  @objc static public func ==(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Bool {
+    return Vec2.==(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
+  }
 
-    public static func ***(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Double {
-        return lhs.wrappedInstance *** rhs.wrappedInstance
-    }
+  @objc static public func ***(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Double {
+    return Vec2.***(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
+  }
+
 }
 
