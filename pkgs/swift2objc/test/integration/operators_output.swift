a773363d30239e4511087dfba1756bbc9a30c223
Hassnaa Mohamed <mhassna727@gmail.com>
2026-02-01 02:29:42 +0200

    tst

--- pkgs/swift2objc/test/integration/operators_output.swift
+++ pkgs/swift2objc/test/integration/operators_output.swift
@@ -31,18 +31,18 @@ import Foundation
     wrappedInstance = Vec2(x: x, y: y)
   }
 
-  @objc static public func +(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Vec2Wrapper {
-    let result = Vec2.+(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
+  public static func +(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Vec2Wrapper {
+    let result = lhs.wrappedInstance + rhs.wrappedInstance
     return Vec2Wrapper(result)
-  }
+    }
 
-  @objc static public func ==(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Bool {
-    return Vec2.==(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
-  }
+  public static func ==(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Bool {
+    return lhs.wrappedInstance == rhs.wrappedInstance
+    }
 
-  @objc static public func ***(lhs lhs: Vec2Wrapper, rhs rhs: Vec2Wrapper) -> Double {
-    return Vec2.***(lhs: lhs.wrappedInstance, rhs: rhs.wrappedInstance)
-  }
+  public static func ***(lhs: Vec2Wrapper, rhs: Vec2Wrapper) -> Double {
+    return lhs.wrappedInstance *** rhs.wrappedInstance
+    }
 
 }
 
